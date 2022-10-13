14f400d973a0c96f5c428c3fc03912e876becc8f
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-13 10:37:20 -0700

    fist c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
+
 /**main- prints the phrase using puts function.
  * returns: zero if done properly and otherwise return non-zero
-*/
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
