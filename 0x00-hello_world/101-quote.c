eee957b59797069b98fe5ebb82b7b11e3e3a689c
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-13 14:36:54 -0700

    Print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,8 +2,10 @@
 #include <unistd.h>
 
 /**
- *main - C program that prints 
- *Return: one if successful
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: zero always (Success)
  */
 int main(void)
 {
