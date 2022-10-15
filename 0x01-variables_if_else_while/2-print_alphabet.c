6c318ffec6811b9fb23435447faf76551bdbf215
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-15 14:12:06 -0700

    print letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase alphabeth using putchar and only twice
+ * Return: zero if successful
+ */
+int main(void)
+{
+	char n;
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
