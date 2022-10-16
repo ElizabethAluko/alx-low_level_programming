7ba8ad9a239d7d79fd3e28821b2af1a92b174f0d
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-15 17:21:15 -0700

    Print number with putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,12 +4,12 @@
  * main - print all single digit numbers from 0 to 9 using putchar.
  * Return - 0 if successful.
  */
-int main(0)
+int main(void)
 {
 	int n;
 
 	for (n = 0; n <= 9; n++)
 		putchar(n);
-	putchar(\n);
+	putchar('\n');
 	return (0);
 }
