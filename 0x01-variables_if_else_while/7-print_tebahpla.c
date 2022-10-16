fb6227fccaec522327c449971b99407f7e6a6852
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-16 09:39:55 -0700

    Print lowercase in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowetcase alphabet in reverse.
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n++)
+		printf("%c\n", n);
+	return (0);
+}
