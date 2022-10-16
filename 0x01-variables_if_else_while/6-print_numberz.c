053dffbe98e433d4ca4b9632c81474b2f93db607
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-15 17:19:05 -0700

    Print number with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit numbers from 0 to 9 using putchar.
+ * Return - 0 if successful.
+ */
+int main(0)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		putchar(n);
+	putchar(\n);
+	return (0);
+}
