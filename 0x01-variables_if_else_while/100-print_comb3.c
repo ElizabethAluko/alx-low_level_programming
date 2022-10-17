a88a381f7d3ff8cf6c78abbf99fa2afa5a947676
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-17 10:25:13 -0700

    Print number combinatios

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible different combinations of two digits
+ * different two digits
+ * smallest combination of two digits all arranged in ascending order.
+ * Return: 0 if possitive
+ */
+int main(void)
+{
+	int i, j;
+	
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			putchar(n);
+			putchar(m);
+			if (i != 57 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
