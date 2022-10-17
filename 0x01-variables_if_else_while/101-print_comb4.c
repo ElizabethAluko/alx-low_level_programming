691fb18a1b2ad1acac2139a4b0059585344c375b
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-17 14:00:54 -0700

    Print three numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,18 +9,18 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 47; i <= 55; i++)
+	for (i = 48; i <= 58; i++)
 	{
-		for (j = 48; j <= 56; j++)
+		for (j = 49; j <= 58; j++)
 		{
-			for (k = 49; k <= 57; k++)
+			for (k = 50; k <= 58; k++)
 			{
 				if (k > j && j > i)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(k);
-					if (i != 55 || j != 56 || k != 57)
+					if (i != 55 || j != 56)
 					{putchar(',');
 						putchar(' ');
 					}
