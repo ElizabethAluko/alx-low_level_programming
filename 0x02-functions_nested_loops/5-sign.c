ca9e21540f496535234834602807fff38bddaaf8
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-19 08:40:14 -0700

    Add function that print sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,12 +14,12 @@ int print_sign(int n)
 		_putchar('+');
 		i = 1;
 	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		_putchar(48);
 		i = 0;
 	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		i = -1;
