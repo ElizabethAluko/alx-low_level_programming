dc5782af6bc98a4e846ae3b2c961a8d4a4db1663
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-19 04:52:18 -0700

    update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints 10 times the alphabet in lowercase.
+ * print_alphabet - prints 10 times the alphabet in lowercase.
  * Return: 0 if successful.
  */
 void print_alphabet(void)
