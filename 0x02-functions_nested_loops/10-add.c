da0add2d46f0e9c283dec0a05ec2f7b168c222bc
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-19 15:04:55 -0700

    add function to add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function that add two integers and return result.
+ * Return: result if successful
+ */
+int add(int i, int j)
+{
+	int i, j, k;
+
+	k = i + j;
+	return (k);
+}
