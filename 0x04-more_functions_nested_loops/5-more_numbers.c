fd2797c394ad241d4ea91da389c5db5ebfad45ce
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-20 17:41:06 -0700

    more function

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,15 @@
  */
 void more_numbers(void)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i < 10; i++)
 	{
-		int j;
-
 		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 			{
+
 				putchar (j/10 + '0');
 			}
 			_putchar(j + '0');
