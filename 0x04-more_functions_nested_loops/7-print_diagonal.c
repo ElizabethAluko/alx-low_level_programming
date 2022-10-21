3d3c1842435dd1dfaaffb7d5d2822967a26c9cac
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-21 14:08:45 -0700

    Add function

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,7 +19,7 @@ void print_diagonal(int n)
 		{
 			if (i == 1)
 			{
-				_putchar('\');
+				_putchar('\\');
 				_putchar('\n');
 			}
 			else
@@ -28,7 +28,7 @@ void print_diagonal(int n)
 				{
 					_putchar(' ');
 				}
-				_putchar('\');
+				_putchar('\\');
 				_putchar('\n');
 			}
 			_putchar('\n');
