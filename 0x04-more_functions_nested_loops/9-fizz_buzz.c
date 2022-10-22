18b183851cb90f5bd49c4cb90c4611bed6974875
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-21 21:12:15 -0700

    Add more function

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -13,17 +14,17 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0 && i % 5 != 0)
 		{
-			printf(" FizzBuzz");
+			printf(" Fizz");
 		}
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 		{
 			printf(" Buzz");
 		}
-		else if (i % 3 == 0)
+		else if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf(" Fizz");
+			printf(" FizzBuzz");
 		}
 
 		else
@@ -32,5 +33,6 @@ int main(void)
 		}
 	}
 	printf("\n");
+
 	return (0);
 }
