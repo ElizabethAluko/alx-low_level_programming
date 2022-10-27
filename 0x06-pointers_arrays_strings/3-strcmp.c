aabef08ef251d1142391ab2ba89e75fc2a1be91e
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-26 23:46:50 -0700

    Add function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,5 +13,28 @@ int _strcmp(char *s1, char *s2)
 	int i = 0;
 	int j = 0;
 
-	while
+	while (s1[i] != '\0')
+	{
+		i++;
+	}
+
+	while (s2[j] != '\0')
+	{
+		j++;
+	}
+
+	if (i = j)
+	{
+		return (0);
+	}
+	
+	else if (i < j)
+	{
+		return (i-j);
+	}
+
+	else if (i > j)
+	{
+		return (i -j);
+	}
 }
