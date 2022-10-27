d92c267529f1233dd1b014caf8f7d7f895ffcb2b
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-26 23:51:10 -0700

    Add function

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * _strcmp - function that compares two strings.
  * @s1: first string.
  * @s2: second string.
- * Returns: 0 if s1 and s2 are equal,
+ * Return: 0 if s1 and s2 are equal,
  * a negative value if s1 is less than s2.
  * a positive value if s1 is greater than s2.
  */
@@ -23,18 +23,18 @@ int _strcmp(char *s1, char *s2)
 		j++;
 	}
 
-	if (i = j)
+	if (i == j)
 	{
 		return (0);
 	}
-	
+
 	else if (i < j)
 	{
-		return (i-j);
+		return (i - j);
 	}
 
 	else if (i > j)
 	{
-		return (i -j);
+		return (i - j);
 	}
 }
