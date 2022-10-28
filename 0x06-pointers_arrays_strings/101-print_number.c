b76be22f29e7e613a9f827bc1616a065b5f6cac4
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-28 13:40:46 -0700

    Add function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,15 +6,18 @@
  */
 void print_number(int n)
 {
+	unsigned int i = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
+		i = -i;
 	}
 
-	if (n / 10 > 0)
+	if (i / 10 > 0)
 	{
-		_putchar(n / 10);
+		_putchar(i / 10);
 	}
 
-	_putchar((n % 10) + '0');
+	_putchar((i % 10) + '0');
 }
