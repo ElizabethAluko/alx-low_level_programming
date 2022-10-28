c08198b6f595bd43c76d426eb49a816332c2ad0e
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-28 13:25:46 -0700

    Add function

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_number - function that print an integer.
+ * @n: integer to print.
+ * Return: 0 if successful.
+ */
+void print_number(int n)
+{
+	_putchar(n);
+	return (0);
+}
