aafb970e5a927bb4e28ca0cf246f539ebf1f4126
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-28 13:33:39 -0700

    Add function

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,10 +3,18 @@
 /**
  * print_number - function that print an integer.
  * @n: integer to print.
- * Return: 0 if successful.
  */
 void print_number(int n)
 {
-	_putchar(n);
-	return (0);
+	if (n < 0)
+	{
+		_putchar('-')
+	}
+
+	if (n / 10 > 0)
+	{
+		_putchar(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
 }
