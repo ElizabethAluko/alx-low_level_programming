2854663ffccd2e6a2ebaabc58f4684115eecc7c1
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-10-31 08:03:40 -0700

    Add function

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte.
+ * @s: pointer to the memory.
+ * @b: constant byte.
+ * @n: bytes of the memory.
+ * Return: a pointer to the memory area s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	n = 0;
+	int i = 0;
+
+	while (i <= n)
+	{
+		s = b;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+# Even more pointers, arrays and strings.
+
+This projects contains function files on pointers, arrays and strings.
