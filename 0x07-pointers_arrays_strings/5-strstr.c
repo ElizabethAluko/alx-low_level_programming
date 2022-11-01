9a34cc2a8d83a2fbc0ad3458aa0998d8feb63d4f
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-01 01:53:37 -0700

    Add function

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -6,18 +6,32 @@
  * @needle: string that will be searched for.
  * Return: pointer to the beginning of the located substring,
  * or NULL if the substring is not found.
+ * if needle is an empty string, haystack itself will be returned
  */
 char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
+	if (needle[0] == '\0')
+		return (haystack);
+
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		if (haystack[i] == needle[0])
 		{
-			if (haystack[i] == needle[j])
-				return (s + i);
+			for (j = 0; needle[j] != '\0'; j++)
+			{
+				if (haystack[i + j] != needle[j])
+				{
+					break;
+				}
+			}
+
+			if (needle[j] == '\0')
+			{
+				return (haystack + i);
+			}
 		}
 	}
-	return (0);
+	return ('\0');
 }
