a96e275fa8956136ad043dbc18432d6bb36d11ce
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-02 21:48:06 -0700

    Add function

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,7 +14,10 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 		return (-1);
 
-	result = x *  _pow_recursion(x, y);
-	y--;
+	if (y == 0)
+		return (1);
+
+	result = x *  _pow_recursion(x, y - 1);
+
 	return (result);
 }
