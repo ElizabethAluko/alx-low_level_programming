d4d554972c6a34f013dc677594d26d32b472f6eb
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-03 00:20:07 -0700

    Add new function

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,10 +7,11 @@
  */
 int is_prime_number(int n)
 {
+	if (n == 1)                                             return (0);
+
 	if (n % 2 == 1)
 		return (1);
-	if (n == 1)
-		return (0);
+
 	else
 		return (0);
 }
