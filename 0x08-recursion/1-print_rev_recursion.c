2a24d2b87ba270dbbda0866598ef9637fd6dce7c
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-03 14:59:56 -0700

    Add function

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,21 +1,5 @@
 #include "main.h"
 
-void _print_rev_recursion(char *s);
-int get_len(char *s);
-
-/**
- * get_len - gets the lenght of the string.
- * @s: string.
- * Return: thd string lenght.
- */
-int get_len(char *s)
-{
-	if (*s == '\0')
-		return (0);
-	else
-		return ((1 + get_len(s + 1)));
-}
-
 
 /**
  * _print_rev_recursion - prints a string in reverse.
@@ -23,18 +7,9 @@ int get_len(char *s)
  */
 void _print_rev_recursion(char *s)
 {
-	int i = 0;
-	char tmp;
-	char len = get_len(s);
-
-	if (s[i] == s[len / 2])
+	if (*s)
 	{
-		tmp = s[i];
-		s[i] = s[len - i - 1];
-		s[len - i - 1] = tmp;
-		i--;
-
-		_print_rev_recursion(*s);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	return (*s);
 }
