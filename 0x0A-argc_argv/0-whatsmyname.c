a2b0affe46e0f6ea9680a446ef806b0facd04934
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-04 05:53:43 -0700

    Add function

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - program that prints its name, followed by a new line.
+ * @argc: the number of arguments in the function.
+ * @argv: the arguments values.
+ * Return: 0 if successful.
+ */
+int main(int argc, char* argv[])
+{
+	_putchar(argv);
+	_putchar('\n');
+	return (0);
+}
