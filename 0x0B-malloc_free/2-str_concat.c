2fa2eebac753dab6b9e0a2b6e2f7cac2f5beb31d
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-08 08:27:24 -0800

    Add malloc task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,10 @@ char *str_concat(char *s1, char *s2)
 	char *nwstr;
 	int i = 0;
 	int j = 0;
+	int k = 0;
+	int l = 0;
 
-	if (s1 == '\0' && s2 == '\0')
+	if (s1[i] == '\0' && s2[j] == '\0')
 		return (0);
 
 	while (s1[i] != '\0')
