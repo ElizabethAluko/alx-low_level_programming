9f9d27f351c1150fcad7dd3c230ee14e60ca175a
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-08 08:15:41 -0800

    Add malloc task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *str_concat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * Return: newly memory space allocated pointer.
+ * return empty if NULL is passed and NULL if failed.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *nwstr;
+	int i = 0;
+	int j = 0;
+
+	if (s1 == NULL && s2 == NULL)
+		return (' ');
+
+	while (s1[i] != '\0')
+		i++;
+	while (s2[j] != '\0')
+		j++;
+
+	nwstr = malloc(sizeof(char) * (i + j) + 1);
+
+	while (k < i)
+	{
+		nwstr[k] = s1[k];
+		k++;
+	}
+	while (k < (i +j))
+	{
+		nwstr[k] = s2[l];
+		k++;
+		l++;
+	}
+	return (nwstr);
+}
