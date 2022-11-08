d4089f3f82a3d7d101ea0775a99d5cc3eacf4bac
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-08 07:31:00 -0800

    Add malloc tasks

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: string.
+ * Return: NULL if str=NUL,
+ * returns a pointer to the duplicated string if successful
+ * and returns NULL if insufficient memory was available.
+ */
+char *_strdup(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char *strcp;
+	
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+		i++;
+
+	strcp = malloc(sizeof(char) * i + 1);
+	
+	if (strcp == NULL)
+		return (NULL);
+
+	while (j < i)
+	{
+		strcp[j] = str[j];
+		j++;
+	}
+	return (strcp);
+	free(strcp);
+}
