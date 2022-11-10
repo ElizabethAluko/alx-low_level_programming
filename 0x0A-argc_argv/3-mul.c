e6f6b93cfdcb4ea57458c178a5395324845f4dfb
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-09 18:10:49 -0800

    Add function

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers and print the result
+ * @argc: number of arguments
+ * @argv: pointer to the arguments.
+ * Return: 0 if successful
+ * If the program does not receive two arguments,
+ * the program should print Error,
+ * followed by a new line, and return 1
+ */
+int __attribute__((unused)) main(int argc, char *argv[])
+{
+	int n1, n2, prd;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n1 = atoi(rgv[1]);
+	n2 = atoi(argv[2]);
+	prd = n1 * n2;
+
+	printf("%d * %d = %d\n", n1, n2, prd);
+	return (0);
+}
