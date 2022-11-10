3b82b8ca1d8c4946cb132dd1215d031b10851cd3
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-10 13:04:11 -0800

    Add function

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -17,9 +17,10 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[2]);
-		printf("%d\n", n1 * n2);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
+		printf("%d\n", mul);
 		return (0);
 	}
 }
