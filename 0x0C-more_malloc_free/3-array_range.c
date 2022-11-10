b577146110e079ba9abb5661709c0e3567a3afac
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-10 11:26:55 -0800

    Add function

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = (int *) malloc(sizeof(int) * size);
+	arr = malloc(sizeof(int) * size);
 
 	if (arr == NULL)
 		return (NULL);
