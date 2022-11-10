7cbd19acdb3f438b97b445506dc05398bb5b8959
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-10 12:47:35 -0800

    Add function

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - multiplies 2 arguments
+ * @argc: number of argument
+ * @argv: array of the argument
+ * Return: 0 if successful.
+ */
+int main(int argc, char *argv[])
+{
+	int n1, n2;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		n1 = atoi(argv[1]);
+		n2 = atoi(argv[2]);
+		printf("%d\n", n1 * n2);
+		return (0);
+	}
+}
