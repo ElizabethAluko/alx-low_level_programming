0012e9d5c9a6b7ee3308db7a768435ea62901e0f
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-14 08:09:14 -0800

    Add preprocessor tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-typedef struct point dog;
-struct dog {
+/**
+ * struct dog - dog data type
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner's name
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
