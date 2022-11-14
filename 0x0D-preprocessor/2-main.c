126b8fc4c095f49c22a6bebda9e90ca5566cd3a4
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-14 06:38:10 -0800

    Add preprocessor tasks

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - prints the name of the file it was compiled
@@ -7,10 +7,9 @@
  * @argv: array of arguments.
  * Return: 0 if successful.
  */
-int __attribute__((unused)) main(int argc, char *argv[])
+int main()
 {
-	(void)argc;
-	printf("%c\n", argv[0]);
+	printf("%s\n", __FILE__);
 	return (0);
 }
 	
