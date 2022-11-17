4eb7a923afea65a4ac738c22884d64d09aeb662d
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-17 05:13:36 -0800

    Add variadic functions task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,12 +9,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list a;
-	int i;
+	unsigned int i;
 	int sum = 0;
 
-	va_start(a, n);
 	if (n == 0)
 		return (0);
+
+	va_start(a, n);
 
 	for (i = 0; i < n; i++)
 		sum += va_arg(a, int);
