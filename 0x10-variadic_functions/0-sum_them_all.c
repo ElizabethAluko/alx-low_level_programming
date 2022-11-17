9b85d34194e24d39341ad763f12166abcee6e8b7
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-17 04:55:45 -0800

    Add variadic functions task

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum of all its parameters.
+ * @n: reference argument.
+ * Return: returns the sum of all the integers.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list a;
+	unsigned int i;
+	unsigned int sum = 0;
+
+	va_start(a, n);
+
+	for (i = n; i >= 0; i++)
+		sum += va_arg(a, unsigned int);
+	va_end(a);
+	return (sum);
+}
