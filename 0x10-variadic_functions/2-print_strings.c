ee8ffb47a23e7b602c0e2207e190e7e0ae91f635
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-17 08:25:58 -0800

    Add task

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,7 +15,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(p, char *);
-		
+
 		if (i > 0 && separator != NULL)
 			printf("%s", separator);
 		if (str == NULL)
