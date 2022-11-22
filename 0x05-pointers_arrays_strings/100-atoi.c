1eaef3918602935cf5ac6ed320ef9f32490e39ab
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-22 07:17:54 -0800

    Add _putchar file

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _atoi - a function that convert a string to an integer.
+ * @s: string to be converted to an integer
+ * Return: 0 if there no number in the string.
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+	int len = 0;
+
