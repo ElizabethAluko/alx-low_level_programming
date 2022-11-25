1731eca6bae3fcae9289d591384af5c8cef2e366
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-24 20:36:25 -0800

    Add advance task1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,8 @@
  *
  * Return: void.
  */
-void before_main()
+void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
 }
