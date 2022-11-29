d29727dad80b56c69b947b933ae5622ae4ef104e
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-29 06:49:09 -0800

    Add task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,7 +15,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
-	while (i <= index)
+	while (head != NULL && i <= index)
 	{
 		ptr = head;
 		head = head->next;
