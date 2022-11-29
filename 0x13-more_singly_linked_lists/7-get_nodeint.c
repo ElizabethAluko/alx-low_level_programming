3490da069896f1507a0eea644262323ded2d01fb
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-11-29 06:45:58 -0800

    Add task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *get_nodeint_at_index - returns the nth node
+ * of a listint_t linked list.
+ * @head: pointer to the list.
+ * @index: index of the node.
+ * Return: returns the nth node of a listint_t linked list.
+ * if the node does not exist, return NULL.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr;
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+	while (i <= index)
+	{
+		ptr = head;
+		head = head->next;
+		i++;
+	}
+	if (i < index)
+		return (NULL);
+	return (ptr);
+}
+
