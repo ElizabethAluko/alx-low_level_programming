cf53a9719704bb38a0c8d887b20e08287f989bed
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-12-02 08:47:57 -0800

    Add task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,8 +3,8 @@
 /**
  * get_bit - returns the value of a bit at a given index.
  * @n: numbet to return its bit at given index.
- * @index: is the index, starting from 0 of the bit you want to get
- * Return: the value of the bit at index index or 
+ * @index: is the index, starting from 0 of the bit you want to get.
+ * Return: the value of the bit at index index or
  * -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
