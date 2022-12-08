8491697acc1254a0495ae59b9d674e5c6d8a391c
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-12-08 09:57:39 -0800

    Add task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,8 @@
  * @text_content: string to write to the file
  *
  * Return: 1 on success, -1 on failure i.e.
- * if file can not be created, file can not be written, write “fails”, etc…
+ * if file can not be created, file can not be
+ * written, write “fails”, etc…
  * The created file must have those permissions: rw-------.
  * If the file already exists, do not change the permissions.
  * if the file already exists, truncate it
