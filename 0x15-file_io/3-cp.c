6bc512bc357a2db32a148bd47681d1005c284ee1
ElizabethAluko <jolaoyebeth24@gmail.com>
2022-12-09 09:33:47 -0800

    Add task 4

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,31 +2,31 @@
 
 /**
  * main - entry point of the progra
- * @ac: number of command line arguments
- * @av: array of command line arguments.
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments.
  *
  * Return: 0 if successf
  */
-int main(int ac, char *av[])
+int main(int argc, char *argv[])
 {
 	int file_from, file_to, len = 1;
 	char buffer[1024];
 
-	if (ac != 3)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
 		exit(97);
 	}
-	file_from = open(av[1], O_RDONLY);
+	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 
@@ -36,7 +36,7 @@ int main(int ac, char *av[])
 
 		if (write(file_to, buffer, len) != len)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
