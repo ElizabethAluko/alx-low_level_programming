8bb48d0bc02d4cfb0a4cee68a5f7d0ba350d0391
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-13 07:08:49 -0800

    Add task7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,7 +38,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			idx--;
 		}
 		else if (ptr->next == NULL && idx != 1)
-			return(NULL);
+			return (NULL);
 		else
 			return (add_dnodeint_end(h, n));
 	}
