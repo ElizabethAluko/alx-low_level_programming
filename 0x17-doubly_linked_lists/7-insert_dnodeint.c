9efd1e9c2687331b2a50253337f9b71c391b7804
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-01-13 05:59:15 -0800

    Add task7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (ptr == NULL)
 		return (NULL);
 
-	while (idx != 1 && ptr->next->next != NULL)
+	while (idx != 1 || ptr->next->next != NULL)
 	{
 		ptr = ptr->next;
 		idx--;
