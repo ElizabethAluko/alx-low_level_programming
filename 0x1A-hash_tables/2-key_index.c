6c6abee2821affebeaab3f9810db9014e7bfe335
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-02-02 23:48:01 -0800

    Add tasks

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of a key.
+ * @key: the key.
+ * @size: The size of the array of the hash table.
+ *
+ * Return: the index at which the key/value pair should be 
+ * stored in the array of the hash table.
+ * Description: Uses hash_djb2 function.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Add/update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add: must not be 1
+ * @value: The key associated with key.
+ *
+ * Return: 1 if successful or 0 upon failure.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new;
+	char *value_c;
+	unsigned long int index, i;
+
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
+	value_c = strdup(value);
+	if (value_c == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key), == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_c);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_c;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
+}
