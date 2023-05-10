3e2ba61d1815143b9467a4ee8c5200192dc90d9a
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-05-10 06:48:10 -0700

    Add task

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -19,15 +19,13 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked arra
+y[%d] = [%d]\n", i, array[i]);
+
 		if (array[i] == value)
 		{
-			return (i);
+			return (i)
 		}
-		else
-		{
-			printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		}
-
 	}
 	return (-1);
 }
